255e33fdf1f88b408a8ecbd5782c09b59a73b1bb
fandipras7 <fandiprasetyopro@gmail.com>
2022-08-18 23:20:45 +0700

    setup heroku dan db

---
+++ src/config/query.sql
@@ -0,0 +1,14 @@
+CREATE TABLE patient (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    created_by VARCHAR (10) DEFAULT 'SYSTEM',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    name VARCHAR(100) NOT NULL,
+    identity_number VARCHAR(100) NOT NULL,
+    address VARCHAR(300) NOT NULL,
+    complaint VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    status VARCHAR(10) DEFAULT 'WAITING'
+
+)
